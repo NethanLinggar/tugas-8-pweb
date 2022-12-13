d0ce16989dea8e8f1b421d11d7396eeae2a64b7c
Nethan Linggar <nethan.linggar@gmail.com>
2022-12-13 23:29:25 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost"; // Nama hostnya
+$username = "user_pweb"; // Username
+$password = "admin123"; // Password 
+$database = "tugas_foto_pweb"; // Nama databasenya
+// Koneksi ke MySQL dengan PDO
+$pdo = null;
+try{
+    $pdo = new PDO('mysql:host=' . $host . ';dbname=' . $database, $username, $password);
+}
+catch(PDOException $e){
+    echo('Connecition failed');
+}
+?>
